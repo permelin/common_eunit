c52678cd8c5a601012e02c01fb73c9e71a82c613
Per Melin <p@greendale.se>
2011-04-21 22:12:24 +0200

    Don't reference common_eunit inside test representation
    Refactored and turned most funs inside out.
    The test representation handed over to EUnit should not longer
    contain any funs that call functions in common_eunit.
    
    This version puts Dialyzer in an infinite loop.

--- src/common_eunit.erl
+++ src/common_eunit.erl
@@ -8,81 +8,71 @@
                    | {group, atom()}.
 
 -type test_rep()  :: {{module(), atom(), arity()}, fun()} 
-                   | {setup, fun(), fun()}
-                   | {setup, fun(), fun(), fun()}.
+                   | {'setup', fun(), fun()}
+                   | {'setup', fun(), fun(), fun()}
+                   | {atom(), test_rep()}
+                   | {atom(), term(), test_rep()}.
 
 -type config()    :: [proplists:property()].
 
 -type control()   :: tuple()
                    | atom().
 
+-type test_fun()  :: fun((config()) -> test_rep()).
+
 
 -spec test(module()) -> 'ok' | 'error'.
 test(Module) ->
     test(Module, Module:all()).
 
--spec test(module(), [test_name()]) -> 'ok' | 'error'.
+-spec test(module(), test_name() | [test_name()]) -> 'ok' | 'error'.
 test(Module, Cases) ->
     eunit:test(test_generator(test_module_name(Module), Cases)).
 
--spec test_generator(module(), [test_name()]) -> test_rep().
+-spec test_generator(module(), test_name() | [test_name()]) -> test_rep().
 test_generator(Module, Cases) ->
     Control = case erlang:function_exported(Module, suite, 0) of
         true  -> Module:suite();
         false -> []
     end,
-    % EUnit test representation of all the cases.
-    SuiteRep = fun(Config) ->
-        TestReps = expand_cases(Module, Cases, Config),
-        control_wrapper(Control, TestReps)
-    end,
-    % Wrap the suite with setup and cleanup functions.
-    % Note that init_per_suite/1 starts with an empty config.
-    Setup   = fun()       -> Module:init_per_suite([]) end,
+    TestFun = control_wrapper(Control, expand_cases(Module, Cases)),
+    Setup   = fun(Config) -> Module:init_per_suite(Config) end,
     Cleanup = fun(Config) -> Module:end_per_suite(Config) end,
-    setup_wrapper(Module, [], SuiteRep, {Setup, init_per_suite, 1},
-                                        {Cleanup, end_per_suite, 1}).
+    SuiteFun = setup_wrapper(Module, TestFun, {Setup, init_per_suite, 1},
+                                              {Cleanup, end_per_suite, 1}),
+    % Start with an empty config, return the full test representation.
+    SuiteFun([]).
 
--spec expand_cases(module(), [test_name()], config()) -> [test_rep()].
-expand_cases(Module, Cases, Config) ->
+-spec expand_cases(module(), test_name() | [test_name()]) -> test_fun().
+expand_cases(Module, Cases) ->
     if is_list(Cases) ->
-            [ expand_case(Module, Case, Config) || Case <- Cases ];
-       is_atom(Cases) ->
-           expand_cases(Module, [Cases], Config)
+            TestFuns = [ expand_case(Module, Case) || Case <- Cases ],
+            fun(Config) -> [ F(Config) || F <- TestFuns ] end;
+       is_atom(Cases); is_tuple(Cases) ->
+           expand_cases(Module, [Cases])
     end.
 
--spec expand_case(module(), test_name(), config()) -> test_rep().
-expand_case(Module, {group, GroupName}, Config0) ->
-    % We're dealing with a group of tests.
-    {Control, Cases} = group_specification(Module, GroupName),
-    % The group's test representation.
-    GroupRep = fun(Config1) ->
-        TestReps = expand_cases(Module, Cases, Config1),
-        control_wrapper(Control, TestReps)
-    end,
-    % Setup and cleanup. Config is passed on down from suite, and any
-    % parent group, setup.
-    Setup   = fun()        -> Module:init_per_group(GroupName, Config0) end,
-    Cleanup = fun(Config1) -> Module:end_per_group(GroupName, Config1) end,
-    setup_wrapper(Module, Config0, GroupRep, {Setup, init_per_group, 2},
-                                             {Cleanup, end_per_group, 2});
-expand_case(Module, CaseName, Config0) ->
-    Test = fun(Config1) ->
+-spec expand_case(module(), test_name()) -> test_fun().
+expand_case(Module, CaseName) when is_atom(CaseName) ->
+    TestFun = fun(Config) ->
         % There are several ways to represent a test in EUnit. We use this:
         % {{M, F, A}, Fun/0}
         % It has the advantage that it lets EUnit give good feedback about
         % exactly which test function is being executed.
-        {{Module, CaseName, 1}, fun() -> apply(Module, CaseName, [Config1]) end}
+        {{Module, CaseName, 1}, fun() -> apply(Module, CaseName, [Config]) end}
     end,
-    % Setup and cleanup. Config is passed on down from suite and group setup.
-    Setup = fun() ->
-        Module:init_per_testcase(CaseName, Config0)
-    end,
-    Cleanup = fun(Config1) ->
-        Module:end_per_testcase(CaseName, Config1)
-    end,
-    setup_wrapper(Module, Config0, Test, {Setup, init_per_testcase, 2},
-                                         {Cleanup, end_per_testcase, 2}).
+    Setup   = fun(Config) -> Module:init_per_testcase(CaseName, Config) end,
+    Cleanup = fun(Config) -> Module:end_per_testcase(CaseName, Config) end,
+    setup_wrapper(Module, TestFun, {Setup, init_per_testcase, 2},
+                                   {Cleanup, end_per_testcase, 2});
+expand_case(Module, {group, GroupName}) ->
+    % We're dealing with a group of tests.
+    {Control, Cases} = group_specification(Module, GroupName),
+    TestFun = control_wrapper(Control, expand_cases(Module, Cases)),
+    Setup   = fun(Config) -> Module:init_per_group(GroupName, Config) end,
+    Cleanup = fun(Config) -> Module:end_per_group(GroupName, Config) end,
+    setup_wrapper(Module, TestFun, {Setup, init_per_group, 2},
+                                   {Cleanup, end_per_group, 2}).
 
 %% Control properties can be either one of these from Common Test:
 %% - parallel
@@ -90,45 +80,54 @@ expand_case(Module, CaseName, Config0) ->
 %% - {timetrap, Time}
 %% ...or any of http://www.erlang.org/doc/apps/eunit/chapter.html#Control
 %%
--spec control_wrapper([control()], [test_rep()]) -> test_rep() | [test_rep()].
-control_wrapper([parallel|T], Tests) ->
-    {inparallel, control_wrapper(T, Tests)};
-control_wrapper([sequence|T], Tests) ->
-    {inorder, control_wrapper(T, Tests)};
-control_wrapper([{timetrap, Time}|T], Tests) ->
-    Seconds = case Time of
-        {hours, Hs}   -> Hs * 60 * 60;
-        {minutes, Ms} -> Ms * 60;
-        {seconds, Ss} -> Ss;
-        MSs           -> MSs / 1000
-    end,
-    {timeout, Seconds, control_wrapper(T, Tests)};
-control_wrapper([Control|T], Tests) when is_tuple(Control) ->
-    % {inparallel, 5} is turned into {inparallel, 5, Tests}
-    list_to_tuple(tuple_to_list(Control) ++ [control_wrapper(T, Tests)]);
-control_wrapper([Control|T], Tests) when is_atom(Control) ->
-    % 'inparallel' is turned into {inparallel, Tests}
-    {Control, control_wrapper(T, Tests)};
-control_wrapper([], Tests) ->
-    Tests.
+-spec control_wrapper([control()], test_fun()) -> test_fun().
+control_wrapper([Control|T], TestFun0) ->
+    TestFun1 = control_wrapper(T, TestFun0),
+    fun(Config) ->
+        case Control of
+            parallel ->
+                {inparallel, TestFun1(Config)};
+            sequence ->
+                {inorder, TestFun1(Config)};
+            {timetrap, Time} ->
+                Seconds = case Time of
+                    {hours, Hs}   -> Hs * 60 * 60;
+                    {minutes, Ms} -> Ms * 60;
+                    {seconds, Ss} -> Ss;
+                    MSs           -> MSs / 1000
+                end,
+                {timeout, Seconds, TestFun1(Config)};
+            C when is_atom(C) ->
+                {C, TestFun1(Config)};
+            {C, Arg} ->
+                {C, Arg, TestFun1(Config)}
+        end
+    end;
+control_wrapper([], TestFun) ->
+    TestFun.
 
 %% Maybe wrap a test fun in an EUnit setup tuple. If we wrap it and exactly
 %% with what depend on if the module exports the right init_per_* and
 %% end_per_* functions.
 %%
--spec setup_wrapper(module(), config(), fun(), maybe(), maybe()) -> test_rep().
--type maybe() :: {fun(), atom(), arity()}.
-setup_wrapper(Module, Config, Tests, {SF, SE, SA}, {CF, CE, CA}) ->
+-spec setup_wrapper(module(), test_fun(), setup(), cleanup()) -> test_fun().
+-type setup()   :: {fun((config()) -> config()), atom(), arity()}.
+-type cleanup() :: {fun((config()) -> _),        atom(), arity()}.
+setup_wrapper(Module, TestFun, {SF, SE, SA}, {CF, CE, CA}) ->
     case erlang:function_exported(Module, SE, SA) of
         true ->
             case erlang:function_exported(Module, CE, CA) of
                 true ->
-                    {setup, SF, CF, Tests};
+                    fun(Config) ->
+                        {setup, fun() -> SF(Config) end, CF, TestFun}
+                    end;
                 false ->
-                    {setup, SF, Tests}
+                    fun(Config) ->
+                        {setup, fun() -> SF(Config) end, TestFun}
+                    end
             end;
         false ->
-            Tests(Config)
+            TestFun
     end.
 
 %% Calls Module:groups() and extracts the specification for the given group.
@@ -156,4 +155,3 @@ test_module_name(Module) ->
         true  -> Module;
         false -> list_to_atom(atom_to_list(Module) ++ TestModuleSuffix)
     end.
-
