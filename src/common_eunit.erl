d804737a5922769adffc365d1fa13eceb67082d9
Per Melin <p@greendale.se>
2011-04-22 22:08:49 +0200

    Reduce some code duplication

--- src/common_eunit.erl
+++ src/common_eunit.erl
@@ -36,10 +36,8 @@ test_generator(Module, Cases) ->
         false -> []
     end,
     TestFun = control_wrapper(Control, expand_cases(Module, Cases)),
-    Setup   = fun(Config) -> Module:init_per_suite(Config) end,
-    Cleanup = fun(Config) -> Module:end_per_suite(Config) end,
-    SuiteFun = setup_wrapper(Module, TestFun, {Setup, init_per_suite, 1},
-                                              {Cleanup, end_per_suite, 1}),
+    SuiteFun = setup_wrapper(Module, TestFun, {init_per_suite, []},
+                                              {end_per_suite, []}),
     % Start with an empty config, return the full test representation.
     SuiteFun([]).
 
@@ -61,18 +59,14 @@ expand_case(Module, CaseName) when is_atom(CaseName) ->
         % exactly which test function is being executed.
         {{Module, CaseName, 1}, fun() -> apply(Module, CaseName, [Config]) end}
     end,
-    Setup   = fun(Config) -> Module:init_per_testcase(CaseName, Config) end,
-    Cleanup = fun(Config) -> Module:end_per_testcase(CaseName, Config) end,
-    setup_wrapper(Module, TestFun, {Setup, init_per_testcase, 2},
-                                   {Cleanup, end_per_testcase, 2});
+    setup_wrapper(Module, TestFun, {init_per_testcase, [CaseName]},
+                                   {end_per_testcase, [CaseName]});
 expand_case(Module, {group, GroupName}) ->
     % We're dealing with a group of tests.
     {Control, Cases} = group_specification(Module, GroupName),
     TestFun = control_wrapper(Control, expand_cases(Module, Cases)),
-    Setup   = fun(Config) -> Module:init_per_group(GroupName, Config) end,
-    Cleanup = fun(Config) -> Module:end_per_group(GroupName, Config) end,
-    setup_wrapper(Module, TestFun, {Setup, init_per_group, 2},
-                                   {Cleanup, end_per_group, 2}).
+    setup_wrapper(Module, TestFun, {init_per_group, [GroupName]},
+                                   {end_per_group, [GroupName]}).
 
 %% Control properties can be either one of these from Common Test:
 %% - parallel
@@ -106,24 +100,36 @@ control_wrapper([Control|T], TestFun0) ->
 control_wrapper([], TestFun) ->
     TestFun.
 
-%% Maybe wrap a test fun in an EUnit setup tuple. If we wrap it and exactly
-%% with what depend on if the module exports the right init_per_* and
-%% end_per_* functions.
+%% Maybe wrap a test fun in an EUnit setup tuple.
+%% See http://www.erlang.org/doc/apps/eunit/chapter.html#Fixtures
 %%
--spec setup_wrapper(module(), test_fun(), setup(), cleanup()) -> test_fun().
--type setup()   :: {fun((config()) -> config()), atom(), arity()}.
--type cleanup() :: {fun((config()) -> _),        atom(), arity()}.
-setup_wrapper(Module, TestFun, {SF, SE, SA}, {CF, CE, CA}) ->
-    case erlang:function_exported(Module, SE, SA) of
+%% If we wrap and then exactly with what depend on which init_per_* and
+%% end_per_* functions are exported by the module.
+%%
+-spec setup_wrapper(module(), test_fun(), Callback, Callback) -> test_fun()
+        when Callback :: {atom(), list()}.
+setup_wrapper(Module, TestFun, {Setup, SA}, {Cleanup, CA}) ->
+    case erlang:function_exported(Module, Setup, length(SA) + 1) of
         true ->
-            case erlang:function_exported(Module, CE, CA) of
+            case erlang:function_exported(Module, Cleanup, length(CA) + 1) of
                 true ->
-                    fun(Config) ->
-                        {setup, fun() -> SF(Config) end, CF, TestFun}
+                    fun(Config0) ->
+                        {setup,
+                            fun() ->
+                                apply(Module, Setup, SA ++ [Config0])
+                            end,
+                            fun(Config1) ->
+                                apply(Module, Cleanup, CA ++ [Config1])
+                            end,
+                            TestFun}
                     end;
                 false ->
                     fun(Config) ->
-                        {setup, fun() -> SF(Config) end, TestFun}
+                        {setup,
+                            fun() ->
+                                apply(Module, Setup, SA ++ [Config])
+                            end,
+                            TestFun}
                     end
             end;
         false ->
