9b661186e696b0bd4274f5722358de160b8e2bdb
Per Melin <p@greendale.se>
2011-04-27 13:21:36 +0200

    Refactor and fix test properties
    Test properties are applied in the correct order, not the order
    they have in the test suite.
    
    Some properties are applied before the appropriate init_per_
    callback, some after.
    
    Individual test cases can specify its own properties through
    a Testcase/0 callback.
    
    Don't pass unknown properties through to EUnit and hope that
    they are valid.

--- src/common_eunit.erl
+++ src/common_eunit.erl
@@ -4,97 +4,135 @@
          test/2,
          test_generator/2]).
 
-
 %% This is called Config in Common Test. It is the return type of the
 %% init_per_* functions.
 -type fixtures() :: [proplists:property()].
 
 %% A representation of a test set that EUnit understands.
 -type eu_test_rep() :: {{atom(), atom(), arity()}, fun()}
-                     | {'setup', eu_setup(), eu_instantiator()}
-                     | {'setup', eu_setup(), eu_cleanup(), eu_instantiator()}
+                     | {'setup', eu_setup(), eu_instant()}
+                     | {'setup', eu_setup(), eu_cleanup(), eu_instant()}
                      | eu_control().
 
--type eu_setup()        :: fun(()           -> fixtures()).
--type eu_cleanup()      :: fun((fixtures()) -> _).
--type eu_instantiator() :: fun((fixtures()) -> eu_test_rep() | [eu_test_rep()]).
+%% http://www.erlang.org/doc/apps/eunit/chapter.html#Fixtures
+-type eu_setup()    :: fun(()           -> fixtures()).
+-type eu_cleanup()  :: fun((fixtures()) -> _).
+
+%% EUnit test instantiator.
+-type eu_instant()  :: fun((fixtures()) -> eu_test_rep() | [eu_test_rep()]).
 
 %% http://www.erlang.org/doc/apps/eunit/chapter.html#Control
 -type eu_control() :: {atom(), eu_test_rep()}
                     | {atom(), term(), eu_test_rep()}.
 
 %% This is an input type for us, which we transform into eu_control().
--type control() :: atom()             % E.g. 'sequence' or 'parallel'
-                 | {atom(), term()}.  % E.g. {timetrap, {seconds, 30}}
+-type prop() :: atom()             % E.g. 'sequence' or 'parallel'
+              | {atom(), term()}.  % E.g. {timetrap, {seconds, 30}}
 
 %% Test case identification.
 -type case_id() :: atom()            % Name of a test function
                  | {group, atom()}.  % Name of a test group
 
+-type level() :: 'suite' | 'group' | 'testcase'.
+
+
+%%%
+%%% Interface
+%%%
 
 -spec test(atom()) -> 'ok' | 'error'.
 test(Module) ->
     test(Module, Module:all()).
 
--spec test(atom(), case_id() | [case_id()]) -> 'ok' | 'error'.
+-spec test(atom(), [case_id()]) -> 'ok' | 'error'.
 test(Module, Cases) ->
     eunit:test(test_generator(test_module_name(Module), Cases)).
 
--spec test_generator(atom(), case_id() | [case_id()]) -> eu_test_rep().
+-spec test_generator(atom(), [case_id()]) -> eu_test_rep().
 test_generator(Module, Cases) ->
-    Control = case erlang:function_exported(Module, suite, 0) of
+    Props = case erlang:function_exported(Module, suite, 0) of
         true  -> Module:suite();
         false -> []
     end,
-    Instantiator0 = control_wrapper(Control, expand_cases(Module, Cases)),
-    Instantiator1 = setup_wrapper(Module, Instantiator0,
-                                  {init_per_suite, []}, {end_per_suite, []}),
+    Instant0 = expand_cases(Module, Cases),
+    Instant1 = wrap(Module, Instant0, Props, suite, []),
     % Start with no fixtures, return the full test representation.
-    Instantiator1([]).
+    Instant1([]).
 
 
--spec expand_cases(atom(), case_id() | [case_id()]) -> eu_instantiator().
-expand_cases(Module, Cases) when is_list(Cases) ->
-    Instantiators = [ expand_case(Module, Case) || Case <- Cases ],
-    fun(Fixtures) -> [ F(Fixtures) || F <- Instantiators ] end;
+%%%
+%%% Implementation
+%%%
+
+-spec fixture_callbacks(level()) -> {atom(), atom()}.
+fixture_callbacks(suite)    -> {init_per_suite, end_per_suite};
+fixture_callbacks(group)    -> {init_per_group, end_per_group};
+fixture_callbacks(testcase) -> {init_per_testcase, end_per_testcase}.
+
+%% Valid test execution properties, in the order they should be applied.
+%%
+%% The first group is applied before the init_per_ call, the second group
+%% between init_per_ and the underlying test cases.
+%%
+-spec props(level()) -> {[atom()], [atom()]}.
+props(suite)    -> {[timetrap], [parallel, sequence, repeat]};
+props(group)    -> {[timetrap], [parallel, sequence, repeat]};
+props(testcase) -> {[timetrap, parallel, sequence, repeat], []}.
+
+%% Add test properties and calls to the appropriate init_per_ and end_per_.
+%%
+-spec wrap(atom(), eu_instant(), [prop()], level(), list())-> eu_instant().
+wrap(Module, Instant0, Props, Level, Args) ->
+    {InitPer, EndPer} = fixture_callbacks(Level),
+    {PreProps, PostProps} = partition_props(Props, Level),
+    Instant1 = add_props(PostProps, Instant0),
+    Instant2 = add_setup(Module, Instant1, InitPer, EndPer, Args),
+    add_props(PreProps, Instant2).
+
+%% Recurse down through groups and test cases and return fully wrapped
+%% instantiators.
+%%
+-spec expand_cases(atom(), [case_id()]) -> eu_instant().
 expand_cases(Module, Cases) ->
-    expand_cases(Module, [Cases]).
+    Instants = [ expand_case(Module, Case) || Case <- Cases ],
+    fun(Fixtures) -> [ F(Fixtures) || F <- Instants ] end.
 
--spec expand_case(atom(), case_id()) -> eu_instantiator().
+-spec expand_case(atom(), case_id()) -> eu_instant().
 expand_case(Module, Case) when is_atom(Case) ->
-    Instantiator = fun(Fixtures) ->
-        % There are several ways to represent a test in EUnit. We use this:
+    Props = case erlang:function_exported(Module, Case, 0) of
+        true  -> Module:Case();
+        false -> []
+    end,
+    Instant = fun(Fixtures) ->
+        % There are several ways to represent a test case to EUnit. We use:
         % {{M, F, A}, Fun/0}
         % It has the advantage that it lets EUnit give good feedback about
         % exactly which test function is being executed.
         {{Module, Case, 1}, fun() -> apply(Module, Case, [Fixtures]) end}
     end,
-    setup_wrapper(Module, Instantiator,
-                  {init_per_testcase, [Case]}, {end_per_testcase, [Case]});
+    wrap(Module, Instant, Props, testcase, [Case]);
 expand_case(Module, {group, Group}) ->
-    % We're dealing with a group of tests.
-    {Control, Cases} = group_specification(Module, Group),
-    Instantiator = control_wrapper(Control, expand_cases(Module, Cases)),
-    setup_wrapper(Module, Instantiator,
-                  {init_per_group, [Group]}, {end_per_group, [Group]}).
+    {Props, Cases} = group_specification(Module, Group),
+    Instant = expand_cases(Module, Cases),
+    wrap(Module, Instant, Props, group, [Group]).
 
-%% Control properties can be either one of these from Common Test:
-%% - parallel
-%% - sequence
-%% - {timetrap, Time}
-%% ...or any of http://www.erlang.org/doc/apps/eunit/chapter.html#Control
+%% See props/1 and http://www.erlang.org/doc/apps/eunit/chapter.html#Control
 %%
--spec control_wrapper([control()], eu_instantiator()) -> eu_instantiator().
-control_wrapper([Control|T], Instantiator0) ->
-    Instantiator1 = control_wrapper(T, Instantiator0),
-    case Control of
+-spec add_props([prop()], eu_instant()) -> eu_instant().
+add_props([Prop|T], Instant0) ->
+    Instant1 = add_props(T, Instant0),
+    case Prop of
         parallel ->
             fun(Fixtures) ->
-                {inparallel, Instantiator1(Fixtures)}
+                {inparallel, Instant1(Fixtures)}
+            end;
+        {parallel, N} ->
+            fun(Fixtures) ->
+                {inparallel, N, Instant1(Fixtures)}
             end;
         sequence ->
             fun(Fixtures) ->
-                {inorder, Instantiator1(Fixtures)}
+                {inorder, Instant1(Fixtures)}
             end;
         {timetrap, Time} ->
             Seconds = case Time of
@@ -104,70 +142,86 @@ control_wrapper([Control|T], Instantiator0) ->
                 MSs           -> MSs / 1000
             end,
             fun(Fixtures) ->
-                {timeout, Seconds, Instantiator1(Fixtures)}
+                {timeout, Seconds, Instant1(Fixtures)}
             end;
         {repeat, N} ->
             fun(Fixtures) ->
-                [ Instantiator1(Fixtures) || _ <- lists:seq(1, N) ]
-            end;
-        C when is_atom(C) ->
-            fun(Fixtures) ->
-                {C, Instantiator1(Fixtures)}
-            end;
-        {C, Arg} ->
-            fun(Fixtures) ->
-                {C, Arg, Instantiator1(Fixtures)}
+                [ Instant1(Fixtures) || _ <- lists:seq(1, N) ]
             end
     end;
-control_wrapper([], Instantiator) ->
-    Instantiator.
+add_props([], Instant) ->
+    Instant.
 
-%% Maybe wrap a test fun in an EUnit setup tuple.
+%% Partition a list of execution properties into two groups to be applied
+%% before and after the appropriate init_per_ callback. Also orders the
+%% individual properties correctly.
+%%
+-spec partition_props([prop()], level()) -> {[prop()], [prop()]}.
+partition_props(Props, Level) ->
+    {PreKeys, PostKeys} = props(Level),
+    Unfolded = proplists:unfold(Props),
+    Pre = filter_props(PreKeys, Unfolded),
+    Post = filter_props(PostKeys, Unfolded),
+    case Unfolded -- (Pre ++ Post) of
+        [] ->
+            {proplists:compact(Pre), proplists:compact(Post)};
+        [{Illegal, _}|_] ->
+            exit({illegal_property, Illegal})
+    end.
+
+-spec filter_props([atom()], [{atom(), term()}]) -> [{atom(), term()}].
+filter_props([Key|T], Props) ->
+    case lists:keysearch(Key, 1, Props) of
+        {value, P} -> [P|filter_props(T, Props)];
+        false      -> filter_props(T, Props)
+    end;
+filter_props([], _) ->
+    [].
+
+%% Wrap an instantiator in an EUnit setup tuple with callbacks to init_per_ and
+%% end_per_ functions.
 %% See http://www.erlang.org/doc/apps/eunit/chapter.html#Fixtures
 %%
-%% If we wrap and then exactly with what depend on which init_per_* and
-%% end_per_* functions are exported by the module.
-%%
--spec setup_wrapper(atom(), eu_instantiator(), Func, Func) -> eu_instantiator()
-        when Func :: {atom(), list()}.
-setup_wrapper(Module, Instantiator, {Setup, SA}, {Cleanup, CA}) ->
-    case erlang:function_exported(Module, Setup, length(SA) + 1) of
+-spec add_setup(atom(), eu_instant(), atom(), atom(), list()) -> eu_instant().
+add_setup(Module, Instant, InitPer, EndPer, Args) ->
+    Arity = length(Args) + 1,
+    case erlang:function_exported(Module, InitPer, Arity) of
         true ->
-            case erlang:function_exported(Module, Cleanup, length(CA) + 1) of
+            case erlang:function_exported(Module, EndPer, Arity) of
                 true ->
                     fun(Fixtures0) ->
                         {setup,
                             fun() ->
-                                apply(Module, Setup, SA ++ [Fixtures0])
+                                apply(Module, InitPer, Args ++ [Fixtures0])
                             end,
                             fun(Fixtures1) ->
-                                apply(Module, Cleanup, CA ++ [Fixtures1])
+                                apply(Module, EndPer, Args ++ [Fixtures1])
                             end,
-                            Instantiator}
+                            Instant}
                     end;
                 false ->
                     fun(Fixtures) ->
                         {setup,
                             fun() ->
-                                apply(Module, Setup, SA ++ [Fixtures])
+                                apply(Module, InitPer, Args ++ [Fixtures])
                             end,
-                            Instantiator}
+                            Instant}
                     end
             end;
         false ->
-            Instantiator
+            Instant
     end.
 
-%% Calls Module:groups() and extracts the specification for the given group.
+%% Call Module:groups() and extract the specification for the given group.
 %%
-%% Returns both a list of any group specific control properties as well as a
+%% Return both a list of any group specific test properties as well as a
 %% list of all the group's members (test cases and other groups).
 %%
--spec group_specification(atom(), atom()) -> {[control()], [case_id()]}.
+-spec group_specification(atom(), atom()) -> {[prop()], [case_id()]}.
 group_specification(Module, Group) ->
     case lists:keyfind(Group, 1, Module:groups()) of
-        {_, Control, Cases} when is_list(Control), is_list(Cases) ->
-            {Control, Cases};
+        {_, Props, Cases} when is_list(Props), is_list(Cases) ->
+            {Props, Cases};
         {_, Cases} when is_list(Cases) ->
             {[], Cases};
         false ->
@@ -176,6 +230,8 @@ group_specification(Module, Group) ->
             exit({bad_group_spec, Group})
     end.
 
+%% Add a _tests suffix to the module name if missing.
+%%
 -spec test_module_name(atom()) -> atom().
 test_module_name(Module) ->
     TestModuleSuffix = "_tests",
--- tests/misc_tests.erl
+++ tests/misc_tests.erl
@@ -3,14 +3,15 @@
 -include_lib("common_eunit/include/common_eunit.hrl").
 
 %%%
-%%% Primarily tests groups, nested groups and special control instructions.
+%%% Primarily tests groups, nested groups and test execution properties.
 %%%
 
 suite() ->
-    [{repeat, 2}, {inparallel, 8}, {timeout, 1}].
+    [{repeat, 2}, {parallel, 8}, {timetrap, {seconds, 1}}].
 
 all() ->
     [simple,
+     single_repeating,
      {group, main},
      {group, ordering},
      {group, many},
@@ -21,7 +22,7 @@ groups() ->
      {nested, [simple]},
      {ordering, [sequence], [{group, one_at_a_time}, {group, all_at_once}]},
      {one_at_a_time, [{repeat, 5}], [singleton, singleton]},
-     {all_at_once, [parallel, {timeout, 0.2}, {repeat, 10}], [slow]},
+     {all_at_once, [parallel, {timetrap, 200}, {repeat, 10}], [slow]},
      {many, [{repeat, 50}], [many]},
      {empty, [], []}].
 
@@ -30,11 +31,13 @@ groups() ->
 
 init_per_suite(Config) ->
     ets:new(test_state, [named_table, public]),
-    ets:insert(test_state, [{many, 0}, {seq, 0}, {simple, 0}, {slow, 0}]),
+    ets:insert(test_state, [{many, 0}, {repeater, 0}, {seq, 0}, {simple, 0},
+                            {slow, 0}]),
     Config.
 
 end_per_suite(_Config) ->
-    ?assertEqual([{many, 100}, {seq, 20}, {simple, 6}, {slow, 20}],
+    ?assertEqual([{many, 100}, {repeater, 20}, {seq, 20}, {simple, 6},
+                  {slow, 20}],
                  lists:sort(ets:tab2list(test_state))).
 
 
@@ -42,6 +45,11 @@ end_per_suite(_Config) ->
 
 simple(_Conf) ->
     ets:update_counter(test_state, simple, 1).
+
+single_repeating() -> [{timetrap, 200}, {repeat, 10}, parallel].
+single_repeating(_Conf) ->
+    ets:update_counter(test_state, repeater, 1),
+    timer:sleep(100).
 
 many(_Conf) ->
     ets:update_counter(test_state, many, 1).
